d789bc84a4dd332e9f16457c153ce5d048c0ffea
long <flashlpro@gmail.com>
2022-03-12 18:44:59 +0700

    them getProfile

--- controllers/public.js
+++ controllers/public.js
@@ -4,14 +4,25 @@ const User = require("../models/User");
 const Profile = require("../models/Profile");
 const Photo = require("../models/Photo");
 
-exports.getAllUser = (req, res, next) => {
+exports.getAllUser = async (req, res, next) => {
     User.find({}, function(err,users) {
       return res.end(JSON.stringify(users));
     });
   };
 
-exports.getAllProfile = (req, res, next) => {
+exports.getAllProfile = async (req, res, next) => {
   Profile.find({}, function(err,profiles) {
     return res.end(JSON.stringify(profiles));
+  });
+};
+
+exports.getProfile = async (req, res, next) => {
+  var profileId = req.params.id;
+  Profile.findOne({_id: profileId}, function(err, profile) {
+    return res.status(200)
+    .json({
+      success: true,
+      profile,
+    });
   });
 };
--- routes/public.js
+++ routes/public.js
@@ -5,10 +5,12 @@ const { protect } = require("../middleware/auth");
 const { 
     getAllUser,
     getAllProfile,
+    getProfile,
 } = require("../controllers/public");
 
 router.route("/alluser").get(getAllUser);
 router.route("/allprofile").get(getAllProfile);
+router.route("/profile/:id").get(getProfile);
 
 
 module.exports = router;
